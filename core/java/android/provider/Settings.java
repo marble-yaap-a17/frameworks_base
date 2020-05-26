21ed7da139bb4acb3c1a265131a71ddd858c5b28
althafvly <althafvly@gmail.com>
2020-05-26 21:17:59 +0800

    SystemUI: Support black theme for dark mode
    Co-authored-by: Jesse Chan <jc@lineageos.org>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I57cd53de8f2c1e4d445441b514875b6af915b858

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14571,6 +14571,12 @@ public final class Settings {
         public static final String VBOOT_HASH = "vboot_hash";
 
         /**
+         * Whether to use black theme for dark mode
+         * @hide
+         */
+        public static final String BERRY_BLACK_THEME = "berry_black_theme";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
