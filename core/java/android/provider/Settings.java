cf0441e2e0e418a1073df9280f604345702c9d87
Jesse Chan <jc@lineageos.org>
2025-03-24 10:45:29 +0000

    SystemUI: Implement hide gestural navigation hint bar
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7663,6 +7663,12 @@ public final class Settings {
         public static final String BATTERY_TEXT_ONLY = "battery_text_only";
 
         /**
+         * boolean value. toggles swipe up hint in gestural nav mode
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_HINT = "navigation_bar_hint";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
